f3db143a2e1bc294f4a5fbdb220abb577b69a5a3
ZhuiCode <zhui890530@outlook.com>
2024-07-04 14:26:31 +0800

    byte.Buffer和byte.Read

--- std/bytes/bytes.go
+++ std/bytes/bytes.go
@@ -13,7 +13,20 @@ func main() {
 	c[0] = 'd'
 	fmt.Printf("####[%s]\n", b)
 	fmt.Printf("####[%s]\n", c)
+
+	//byte.Buffer
 	buf := bytes.NewBuffer(b)
 	fmt.Printf("####[%d]\n", buf.Len())
 	fmt.Printf("####[%s]\n", b)
+	//flag := bytes.Contains(b, []byte("a"))
+	s := []byte("hello world")
+	fmt.Println(bytes.HasPrefix(s, []byte("el")))
+	d := bytes.NewBuffer(s)
+	fmt.Println(d.Len())
+
+	//byte.Reader --只读操作
+	r := bytes.NewReader(s) //将s的内容拷贝到r中
+	f := make([]byte, 5)    //申请一块空间f
+	r.Read(f)               //将r中的空间读入到f
+	fmt.Println(string(f))
 }
